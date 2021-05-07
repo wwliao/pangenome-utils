6a3c030ef84b88396fdbf13045f2feadef504edf
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2021-05-07 16:16:38 -0400

    Add a script used to assign node colors in CSV format for visualizing in Bandage

---
+++ create_bandage_csv_labels.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# CSV format: https://github.com/rrwick/Bandage/wiki/CSV-labels
+import re
+import argparse
+from os.path import basename, splitext
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--rank", type=int, default=0)
+parser.add_argument("-c", "--color", default="#4CB391")
+parser.add_argument("gfafile")
+args = parser.parse_args()
+
+prefix = splitext(basename(args.gfafile))[0]
+with open(args.gfafile) as infile, open(f"{prefix}.labels.csv", "w") as outfile:
+    outfile.write("Node,Color\n")
+    pattern = re.compile("^S\t(\S+)\t(\S+)(\t.*)*")
+    for line in infile:
+        m = pattern.search(line)
+        if m:
+            node = m[1]
+            tags = m[3]
+            if tags:
+                m = re.search(f"\tSR:i:{args.rank}", tags)
+                if m:
+                    outfile.write(f"{node},{args.color}\n")
+                else:
+                    outfile.write(f"{node},#BEBEBE\n")
