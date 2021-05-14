50c79dcb1dc6c1f8c1277a0ecd2db1e0b08ab098
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2021-05-14 10:47:06 -0400

    Add a script used to convert W-line to P-line in GFA with an option for stripping rGFA tags

---
+++ convert_wline_to_pline.py
@@ -0,0 +1,39 @@
+import re
+import argparse
+from os.path import splitext, basename
+from collections import Counter
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--striptags", action="store_true")
+parser.add_argument("gfafile")
+args = parser.parse_args()
+
+root, ext = splitext(basename(args.gfafile))
+counter = Counter()
+with open(args.gfafile) as infile, open(f"{root}.w2p{ext}", "w") as outfile:
+    for line in infile:
+        if line.startswith("W"):
+            cols = line.strip().split("\t")
+            contig = f"{cols[1]}#{cols[2]}#{cols[3]}"
+            counter.update([contig])
+            count = counter[contig]
+            pathname = f"{contig}#{count}"
+            walk = cols[6]
+            path = []
+            for match in re.finditer("([><])(\d+)", walk):
+                node = match.group(2)
+                if match.group(1) == ">":
+                    direct = "+"
+                else:
+                    direct = "-"
+                path.append(f"{node}{direct}")
+            pathstr = ",".join(path)
+            outfile.write(f"P\t{pathname}\t{pathstr}\t*\n")
+        elif args.striptags and line.startswith("S"):
+            segment = "\t".join(line.strip().split("\t")[:3]) + "\n"
+            outfile.write(segment)
+        elif args.striptags and line.startswith("L"):
+            link = "\t".join(line.strip().split("\t")[:6]) + "\n"
+            outfile.write(link)
+        else:
+            outfile.write(line)
