11a9f5ae274c2c10852d41f58234774ec75b97cb
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2021-06-02 22:13:04 -0400

    Update filter_traversal_pairs.py

--- filter_traversal_pairs.py
+++ filter_traversal_pairs.py
@@ -3,6 +3,7 @@ import argparse
 from os.path import basename, splitext
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--rmref", action="store_true", help="Remove record with 0|0 genotype")
 parser.add_argument("pairs", help="Traversal pairs in BEDPE format")
 args = parser.parse_args()
 
@@ -12,6 +13,12 @@ with open(args.pairs) as infile, open(f"{root}.filtered{ext}", "w") as outfile:
         if i == 0:
             outfile.write(line)
         else:
-            filter = line.strip().split("\t")[10]
-            if filter == ".":
-                outfile.write(line)
+            cols = line.strip().split("\t")
+            filter = cols[9]
+            genotype = cols[10]
+            if args.rmref:
+                if filter == "." and genotype != "0|0":
+                    outfile.write(line)
+            else:
+                if filter == ".":
+                    outfile.write(line)
