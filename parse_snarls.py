3ba9346cb5c0f954c0ffc43e21728bf8bca6f9df
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2021-06-15 21:39:24 -0400

    Update parse_snarls.py

--- parse_snarls.py
+++ parse_snarls.py
@@ -21,34 +21,35 @@ with open(args.ref_nodes) as infile:
 with open(args.snarls) as infile, open(args.output, "w") as outfile, open("filterout.bedpe", "w") as filterout:
     for line in infile:
         snarl = json.loads(line)
-        if "parent" not in snarl and "type" in snarl:
-            if snarl["type"] == 1:
-                node1 = snarl["start"]["node_id"]
-                node2 = snarl["end"]["node_id"]
-                if node1 in ref and node2 in ref:
-                    if "backward" not in snarl["start"] and "backward" not in snarl["end"]:
-                        source = node1
-                        sink = node2
-                        snarl_id = f">{source}>{sink}"
-                    elif "backward" in snarl["start"] and "backward" in snarl["end"]:
-                        source = node2
-                        sink = node1
-                        snarl_id = f">{source}>{sink}"
+        #if "parent" not in snarl and "type" in snarl:
+            #if snarl["type"] == 1:
+        if "parent" not in snarl:
+            node1 = snarl["start"]["node_id"]
+            node2 = snarl["end"]["node_id"]
+            if node1 in ref and node2 in ref:
+                if "backward" not in snarl["start"] and "backward" not in snarl["end"]:
+                    source = node1
+                    sink = node2
+                    snarl_id = f">{source}>{sink}"
+                elif "backward" in snarl["start"] and "backward" in snarl["end"]:
+                    source = node2
+                    sink = node1
+                    snarl_id = f">{source}>{sink}"
+                else:
+                    source = node1
+                    sink = node2
+                    if "backward" in snarl["start"]:
+                        snarl_id = f"<{source}>{sink}"
                     else:
-                        source = node1
-                        sink = node2
-                        if "backward" in snarl["start"]:
-                            snarl_id = f"<{source}>{sink}"
-                        else:
-                            snarl_id = f">{source}<{sink}"
+                        snarl_id = f">{source}<{sink}"
 
-                    chrom1 = ref[source][0]
-                    start1 = ref[source][1]
-                    end1 = ref[source][2]
-                    chrom2 = ref[sink][0]
-                    start2 = ref[sink][1]
-                    end2 = ref[sink][2]
-                    if chrom1 == chrom2 and start2 >= end1:
-                        outfile.write(f"{chrom1}\t{start1}\t{end1}\t{chrom2}\t{start2}\t{end2}\t{snarl_id}\n")
-                    else:
-                        filterout.write(f"{chrom1}\t{start1}\t{end1}\t{chrom2}\t{start2}\t{end2}\t{snarl_id}\n")
+                chrom1 = ref[source][0]
+                start1 = ref[source][1]
+                end1 = ref[source][2]
+                chrom2 = ref[sink][0]
+                start2 = ref[sink][1]
+                end2 = ref[sink][2]
+                if chrom1 == chrom2 and start2 >= end1:
+                    outfile.write(f"{chrom1}\t{start1}\t{end1}\t{chrom2}\t{start2}\t{end2}\t{snarl_id}\n")
+                else:
+                    filterout.write(f"{chrom1}\t{start1}\t{end1}\t{chrom2}\t{start2}\t{end2}\t{snarl_id}\n")
